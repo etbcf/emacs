6ea864177f3a1e423839af3f8ef36453dd0f7160
etbcf <tarik.edu2@gmail.com>
2023-12-21 08:24:15 +0000

    Initial commit.

---
+++ init.el
@@ -0,0 +1,43 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+
+;;; setup package.el
+;; --------------------------------------------------------------------------------
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(unless package--initialized (package-initialize))
+
+
+
+;;; setup use-package
+;; --------------------------------------------------------------------------------
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+(setq use-package-always-ensure t)
+
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
+
+
+(provide 'init.el)
+;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package cmake-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
